dcdb9ce24a0f135a95cec2ef6ca8fdf2dd742308
neyzar1111 <choka95@hotmail.com>
2021-07-13 14:13:30 +0200

    Fix carousel

--- script-bob.js
+++ script-bob.js
@@ -1,29 +1,29 @@
 // CARROUSEL BOOTSTRAP TEMPLATE ROWS
 
-let intervalo;
-const rows = document.querySelectorAll('.scroller');
-
-let handlePrev = document.querySelectorAll(".handlePrev");
-let handleNext = document.querySelectorAll(".handleNext");
-
-handlePrev.forEach((e,i)=>e.addEventListener('click',function toLeftScroll  (){
-    intervalo = setInterval(function(){ rows[i].scrollLeft -= 1 }  , 2);
-
-} ));
-
-handleNext.forEach((e,i)=>e.addEventListener('click',function toRightScroll (){
-    intervalo = setInterval(function(){ rows[i].scrollLeft += 1 }  , 2);
-} ));
-
-
-handlePrev.forEach((e,i)=>e.addEventListener("mouseleave", function clearScroll(){
-    clearInterval(intervalo);
-
-}));
-handleNext.forEach((e,i)=>e.addEventListener("mouseleave", function clearScroll(){
-    clearInterval(intervalo);
-
-}));
+// let intervalo;
+// const rows = document.querySelectorAll('.scroller');
+//
+// let handlePrev = document.querySelectorAll(".handlePrev");
+// let handleNext = document.querySelectorAll(".handleNext");
+//
+// handlePrev.forEach((e,i)=>e.addEventListener('click',function toLeftScroll  (){
+//     intervalo = setInterval(function(){ rows[i].scrollLeft -= 1 }  , 2);
+//
+// } ));
+//
+// handleNext.forEach((e,i)=>e.addEventListener('click',function toRightScroll (){
+//     intervalo = setInterval(function(){ rows[i].scrollLeft += 1 }  , 2);
+// } ));
+//
+//
+// handlePrev.forEach((e,i)=>e.addEventListener("mouseleave", function clearScroll(){
+//     clearInterval(intervalo);
+//
+// }));
+// handleNext.forEach((e,i)=>e.addEventListener("mouseleave", function clearScroll(){
+//     clearInterval(intervalo);
+//
+// }));
 
 /* OLD DROP DOWN FUNCTION
 function dropDownFunction() {
--- scripts/script-choka.js
+++ scripts/script-choka.js
@@ -9,3 +9,36 @@ makeSliderRow(sectionAllMovies);
 makeSliderRow(sectionAllMovies);
 let cards = document.querySelectorAll(".gui-card");
 console.log(cards.length);
+
+
+const rows = document.querySelectorAll('.scroller');
+let handlePrev = document.querySelectorAll(".handlePrev");
+let handleNext = document.querySelectorAll(".handleNext");
+
+handlePrev.forEach((e,i)=>e.addEventListener('click',function toLeftScroll  (){
+    rows[i].scrollLeft -= 120;
+
+} ));
+
+handleNext.forEach((e,i)=>e.addEventListener('click',function toRightScroll (){
+    rows[i].scrollLeft += 120;
+} ));
+
+
+
+
+// API key
+//2fc09aa4868aaf55aacca5023aea7172
+//img urlgit
+//https://image.tmdb.org/t/p/w500
+fetch("\n" +
+    "https://api.themoviedb.org/3/movie/popular?api_key=2fc09aa4868aaf55aacca5023aea7172&language=en-US")
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        console.log(data)
+    })
+    .catch(err => {
+        console.error(err);
+    });
