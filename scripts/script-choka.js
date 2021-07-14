30947cc23814f1ad7cf8bad1e54a28d90d052881
BobW <biz.wautersbob@gmail.com>
2021-07-14 16:42:19 +0200

    Added row 3 + minor changes

--- scripts/script-choka.js
+++ scripts/script-choka.js
@@ -80,6 +80,7 @@ const setImagesForPopular = (res)=>{
 const titleRow = document.querySelectorAll(".title-title-row");
     titleRow[0].innerHTML = "Popular";
     titleRow[1].innerHTML = "Upcoming";
+    titleRow[2].innerHTML= "Oldies but Goldies";
 
 
 cards.forEach(e=>{
