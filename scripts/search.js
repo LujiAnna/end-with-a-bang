c79560c5b43d79ed2d914d965fc2c6ee06ad7404
neyzar1111 <choka95@hotmail.com>
2021-07-16 13:39:23 +0200

    Fix search input

--- scripts/search.js
+++ scripts/search.js
@@ -58,3 +58,51 @@ let getSearchText = () => {
 
 // user interaction
 searchElement.addEventListener('click', getSearchText);
+
+
+//New code
+
+searchElement.addEventListener('click', getSearchText);
+
+let searchItem=  () =>{
+    let sItem = localStorage.getItem( 'searchInput');
+    return sItem
+}
+searchItem();
+console.log(searchItem())
+
+
+fetch("https://api.themoviedb.org/3/movie/popular?api_key=2fc09aa4868aaf55aacca5023aea7172&language=en-US")
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        // console.log(data.results)
+        findMovieInData(data.results, searchItem());
+
+    })
+    .catch(err => {
+        console.error(err);
+    });
+let arrFind;
+let findMovieInData = (movieDB, searchItem)=>{
+    let regex = new RegExp(searchItem, 'g');
+    // console.log(regex.test(searchItem))
+    // console.log(searchItem)
+    let m = movieDB.filter(e=>{
+       if (e.title.match(regex) !== null){
+           return e;
+       }
+    })
+    arrFind = m;
+    console.log(arrFind)
+    setImageToFound(arrFind)
+    return m ;
+}
+const imgUrl = 'https://image.tmdb.org/t/p/w500';
+const movieTitle = document.querySelector(".gui-card__title");
+const movieImage = document.querySelector(".gui-card__img");
+const setImageToFound = (res) => {
+    movieImage.src = `${imgUrl}${res[0].backdrop_path}`;
+    movieTitle.innerHTML = res[0].title;
+};
